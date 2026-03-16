4246c439f14f4fe746dad310810f714d5fc4a0b5
Martinelli Mickael <mickael.martinelli@student-cs.fr>
2026-03-16 15:54:03 +0100

    Region not allowed

--- 17_robot_mission_MAS2026/model.py
+++ 17_robot_mission_MAS2026/model.py
@@ -4,11 +4,11 @@ from mesa.datacollection import DataCollector
 from mesa import Model
 from mesa.space import MultiGrid
 from agents import GreenRobotAgent, RedRobotAgent, YellowRobotAgent
-from utils import Action 
+from utils import Action, AgentColor 
 
 class RobotMission(Model):
     """A model with some number of agents."""
-    def __init__(self, number_of_green_robots=10, number_of_yellow_robots=10, number_of_red_robots=10, width_z1=10, width_z2=10, width_z3=10, height=30, seed=None):
+    def __init__(self, number_of_green_robots=1, number_of_yellow_robots=1, number_of_red_robots=1, width_z1=10, width_z2=10, width_z3=10, height=30, seed=None):
         """Initialize the model.
 
         Args:
@@ -23,12 +23,23 @@ class RobotMission(Model):
         """
 
         super().__init__(seed=seed)
-        self.grid = MultiGrid(width_z1 + width_z2 + width_z3, height, torus=True)
+        self.width_z1 = width_z1
+        self.width_z2 = width_z2
+        self.width_z3 = width_z3
+        self.grid = MultiGrid(width_z1 + width_z2 + width_z3, height, torus=False)
         agents = [GreenRobotAgent(self) for i in range(number_of_green_robots)] + [YellowRobotAgent(self) for i in range(number_of_yellow_robots)] + [RedRobotAgent(self) for i in range(number_of_red_robots)]
         for agent in agents:
             x = self.random.randrange(width_z1)
             y = self.random.randrange(height)
             self.grid.place_agent(agent, (x, y))
+
+    def get_zone(self, pos):
+        if pos[0] < self.width_z1:
+            return 1
+        elif pos[0] < self.width_z2:
+            return 2
+        else:
+            return 3
 
 
     def step(self):
@@ -36,18 +47,20 @@ class RobotMission(Model):
     
     def do(self, agent, action):
         if action == Action.MOVE_RIGHT:
-            new_position = agent.pos[0], agent.pos[1] + 1
-        elif action == Action.MOVE_LEFT:
-            new_position = agent.pos[0], agent.pos[1] - 1
-        elif action == Action.MOVE_TOP:
-            new_position = agent.pos[0] - 1, agent.pos[1]
-        elif action == Action.MOVE_DOWN:
             new_position = agent.pos[0] + 1, agent.pos[1]
+        elif action == Action.MOVE_LEFT:
+            new_position = agent.pos[0] - 1, agent.pos[1]
+        elif action == Action.MOVE_TOP:
+            new_position = agent.pos[0], agent.pos[1] + 1
+        elif action == Action.MOVE_DOWN:
+            new_position = agent.pos[0], agent.pos[1] - 1
         else:
             new_position = agent.pos
 
         if new_position in self.grid.get_neighborhood(agent.pos, moore=False, include_center=False):
-            self.grid.move_agent(agent, new_position)
+            zone = self.get_zone(new_position)
+            if (agent.color == AgentColor.GREEN and zone <= 1) or (agent.color == AgentColor.YELLOW and zone <= 2) or (agent.color == AgentColor.RED and zone <= 3):
+                self.grid.move_agent(agent, new_position)
 
         return self.grid.get_neighborhood(agent.pos, moore=False, include_center=False)
 
